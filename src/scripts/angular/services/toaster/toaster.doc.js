c88388ccb10784bdfbd994e0cb90e892cf350769
Rafael Camargo <hello@rafaelcamargo.com>
2020-03-30 13:21:22 -0300

    chore: improve toaster documentation (angular)
    add theme toast example

--- src/scripts/angular/services/toaster/toaster.doc.js
+++ src/scripts/angular/services/toaster/toaster.doc.js
@@ -33,6 +33,55 @@ module.exports = {
       <pd-button on-click="$ctrl.popToaster">
         Default Toaster
       </pd-button>`
+    },
+    {
+      title: 'Theme Toast',
+      controller: function(toaster){
+        const $ctrl = this;
+
+        $ctrl.popSuccess = () => {
+          popToast('Success Toast', 'success')
+        };
+
+        $ctrl.popDanger = () => {
+          popToast('Danger Toast', 'danger')
+        };
+
+        $ctrl.popWarning = () => {
+          popToast('Warning Toast', 'warning')
+        };
+
+        function popToast(message, theme){
+          toaster.pop({ message, theme });
+        }
+      },
+      dependencies: ['toaster'],
+      template: `
+      <p-row>
+        <pd-col data-size="4">
+          <pd-button
+            on-click="$ctrl.popSuccess"
+            data-toast-theme="success"
+            data-block="true">
+            Success Toast
+          </pd-button>
+        </pd-col>
+        <pd-col data-size="4">
+          <pd-button
+            on-click="$ctrl.popDanger"
+            data-block="true">
+            Danger Toast
+          </pd-button>
+        </pd-col>
+        <pd-col data-size="4">
+          <pd-button
+            on-click="$ctrl.popWarning"
+            data-block="true">
+            Warning Toast
+          </pd-button>
+        </pd-col>
+      </p-row>
+      `
     }
   ]
 };
