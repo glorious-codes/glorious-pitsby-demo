9344653c28aea13e68c7cab91bf916afe42224bb
Rafael Camargo <hello@rafaelcamargo.com>
2020-03-31 14:21:06 -0300

    feat: create alert documentation (vanilla)

---
+++ src/scripts/vanilla/components/alert/alert.doc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  name: 'Alert',
+  examples: [
+    {
+      title: 'Default Alert',
+      template: `
+      <div class="pd-alert">
+        Default Alert.
+      </div>`,
+    },
+    {
+      title: 'Theme Alert',
+      description: 'You can style a alert according themes.',
+      template: `
+      <div>
+        <div class="pd-row">
+          <div class="pd-col pd-col-12">
+            <div class="pd-alert pd-alert-success">
+              Success Alert.
+            </div>
+          </div>
+        </div>
+        <div class="pd-row">
+          <div class="pd-col pd-col-12">
+            <div class="pd-alert pd-alert-danger">
+              Danger Alert.
+            </div>
+          </div>
+        </div>
+      </div>`,
+    },
+    {
+      title: 'Alert with Trigger',
+      description: 'You can optionally show a button to trigger some action',
+      controller: function(element){
+        const retryButton = element.querySelector('button');
+
+        retryButton.addEventListener('click', () => {
+          alert('trigger clicked!');
+        });
+      },
+      template: `
+      <div class="pd-row">
+        <div class="pd-col pd-col-12">
+          <div class="pd-alert pd-alert-danger">
+            Something went wrong. Please, try again.
+            <button type="button" class="pd-alert-trigger">
+              Retry
+            </button>
+          </div>
+        </div>
+      </div>`,
+    }
+  ]
+};
