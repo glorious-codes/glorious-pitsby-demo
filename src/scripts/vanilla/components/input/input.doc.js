7a0c3e84bfbebd680a5c8ca1384d2be1615a61e0
Rafael Camargo <hello@rafaelcamargo.com>
2020-03-31 14:09:44 -0300

    feat: create input documentation (vanilla)

---
+++ src/scripts/vanilla/components/input/input.doc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'Input',
+  examples: [
+    {
+      title: 'Default Input',
+      template: `
+      <div class="pd-row">
+        <div class="pd-col pd-col-4">
+          <input type="text" class="pd-input" />
+        </div>
+      </div>`
+    },
+    {
+      title: 'Disabled Input',
+      template: `
+      <div class="pd-row">
+        <div class="pd-col pd-col-4">
+          <input type="text" class="pd-input" disabled />
+        </div>
+      </div>`
+    }
+  ]
+};
