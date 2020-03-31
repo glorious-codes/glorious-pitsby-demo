6dab1d2470c019c45377db266b6af71e99485c06
Rafael Camargo <hello@rafaelcamargo.com>
2020-03-31 14:10:12 -0300

    feat: create textarea documentation (vanilla)

---
+++ src/scripts/vanilla/components/textarea/textarea.doc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'Textarea',
+  examples: [
+    {
+      title: 'Default Textarea',
+      template: `
+      <div class="pd-row">
+        <div class="pd-col pd-col-4">
+          <textarea class="pd-textarea"></textarea>
+        </div>
+      </div>`
+    },
+    {
+      title: 'Disabled Textarea',
+      template: `
+      <div class="pd-row">
+        <div class="pd-col pd-col-4">
+          <textarea class="pd-textarea" disabled></textarea>
+        </div>
+      </div>`
+    }
+  ]
+};
