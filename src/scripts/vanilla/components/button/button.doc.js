f43e3ac3ed18b093f76a4571897a496efb64b3c9
Rafael Camargo <hello@rafaelcamargo.com>
2020-03-31 23:58:59 -0300

    feat: improve button (vanilla)
    add secondary theme

--- src/scripts/vanilla/components/button/button.doc.js
+++ src/scripts/vanilla/components/button/button.doc.js
@@ -12,16 +12,39 @@ module.exports = {
       title: 'Theme Button',
       description: 'You can style a button according themes.',
       template: `
-      <button class="pd-button pd-button-primary">
-        Primary Button
-      </button>`
+      <div class="pd-row">
+        <div class="pd-col-3">
+          <button class="pd-button pd-button-primary">
+            Primary Button
+          </button>
+        </div>
+        <div class="pd-col-3">
+          <button class="pd-button pd-button-secondary">
+            Secondary Button
+          </button>
+        </div>
+      </div>`
     },
     {
       title: 'Disabled Button',
       template: `
-      <button class="pd-button pd-button-primary" disabled>
-        Primary Button
-      </button>`
+      <div class="pd-row">
+        <div class="pd-col-3">
+          <button class="pd-button" disabled>
+            Default Button
+          </button>
+        </div>
+        <div class="pd-col-3">
+          <button class="pd-button pd-button-primary" disabled>
+            Primary Button
+          </button>
+        </div>
+        <div class="pd-col-3">
+          <button class="pd-button pd-button-secondary" disabled>
+            Secondary Button
+          </button>
+        </div>
+      </div>`
     },
     {
       title: 'Block Button',
